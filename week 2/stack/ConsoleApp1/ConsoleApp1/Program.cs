e1ad1b81d01f7d2345c1481d13c18e2dc3035161
aimlee <35259280+aimlee@users.noreply.github.com>
2018-01-21 20:31:07 +0600

    Add files via upload

---
+++ week 2/stack/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dirs
+{
+    class Program
+    {
+
+        static void f(string path, int depth = 0)
+        {
+            Stack<DirectoryInfo> myS = new Stack<DirectoryInfo>();
+            DirectoryInfo dir = new DirectoryInfo(path);
+            myS.Push(dir);
+
+            while (myS.Count > depth)
+            {Console.Write("     ");
+                DirectoryInfo cur = myS.Pop();
+                foreach (DirectoryInfo d in cur.GetDirectories())
+                {
+                    
+                    Console.WriteLine(d.Name);
+                    myS.Push(d);
+                }
+
+                foreach (FileInfo f in cur.GetFiles())
+                {
+                    
+                    Console.WriteLine(f.Name);
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            f(@"F:\c# labs");
+
+       
+
+            Console.ReadKey();
+        }
+    }
+}
